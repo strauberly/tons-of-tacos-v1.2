d47676025f0e8d8d771e1164cd6bd335718cc761
Brian Crookshanks <zeron30@hotmail.com>
2023-01-25 21:47:13 -0600

    In memory database research for testing purposes.

--- back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/tonsoftacos/entities/MenuItem.java
+++ back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/tonsoftacos/entities/MenuItem.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
+
 
 @Entity
 @Table(name = "menu_item")
@@ -14,21 +14,32 @@ public class MenuItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Integer id;
 
-    @Column(name = "item_name")
-    private String item_name;
-    @Column (name = "item_size")
-    private String item_size;
-
-    @Column (name = "category")
-    private String category;
-
-    @Column(name = "unit_price")
-    private BigDecimal unit_price;
-    @Column(name = "img_url")
-    private String img_url;
+    @Column (name = "category_type")
+    private String category_type;
 
     @Column(name = "description")
     private String description;
+
+    @Column(name = "img_url")
+    private String img_url;
+
+    @Column(name = "item_name")
+    private String item_name;
+
+    @Column (name = "item_size")
+    private String item_size;
+
+    @Column(name = "unit_price")
+    private Double unit_price;
+
+    public MenuItem(String category_type, String description, String img_url, String item_name, String item_size, Double unit_price) {
+        this.category_type = category_type;
+        this.description = description;
+        this.img_url = img_url;
+        this.item_name = item_name;
+        this.item_size = item_size;
+        this.unit_price = unit_price;
+    }
 }
