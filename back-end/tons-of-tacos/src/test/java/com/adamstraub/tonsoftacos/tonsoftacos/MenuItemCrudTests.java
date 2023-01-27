4ba995d8c10763520052cd946d15c6c5f074dbcc
Brian Crookshanks <zeron30@hotmail.com>
2023-01-27 17:40:23 -0600

    Successful test created for getting a menu item and comparing against expected data

--- back-end/tons-of-tacos/src/test/java/com/adamstraub/tonsoftacos/tonsoftacos/MenuItemCrudTests.java
+++ back-end/tons-of-tacos/src/test/java/com/adamstraub/tonsoftacos/tonsoftacos/MenuItemCrudTests.java
@@ -1,28 +1,36 @@
 package com.adamstraub.tonsoftacos.tonsoftacos;
 import com.adamstraub.tonsoftacos.tonsoftacos.dao.MenuItemRepository;
 import com.adamstraub.tonsoftacos.tonsoftacos.entities.MenuItem;
-import com.adamstraub.tonsoftacos.tonsoftacos.testSupport.GetMenuItemsTestsSupport;
+import com.adamstraub.tonsoftacos.tonsoftacos.testSupport.MenuItemCrudTestsSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @DataJpaTest
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class GetMenuItemsTests extends GetMenuItemsTestsSupport {
-//    ** These are just in memory database tests
+public class MenuItemCrudTests extends MenuItemCrudTestsSupport {
+//    ** CRUD TEST TO BE MIGRATED
     @Autowired
     private MenuItemRepository menuItemRepository;
 
     @Test
 //    @Rollback(false)
-    public void addMenuItemTestToDB(){
+    void addMenuItemTestToDB(){
 //        try builder here
 //        MenuItem menuItem = new MenuItem( "taco", "nom nom", "steve's house", "super taco", null, 3.50);
-//        MenuItem savedItem = menuItemRepository.save(menuItem);
-//        assertNotNull(savedItem);
+
+        MenuItem savedItem = menuItemRepository.save(sample());
+        assertNotNull(savedItem);
     }
 
+    @Test
+    void getItemFromDatabase(){
 
+    }
 
 }
