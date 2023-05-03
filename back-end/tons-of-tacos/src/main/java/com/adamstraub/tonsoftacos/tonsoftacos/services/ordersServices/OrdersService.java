eec8ca8efc7d9b6c8d4d5b4f1b4b831eb67e1e57
Brian Crookshanks <zeron30@hotmail.com>
2023-05-02 20:48:13 -0700

    cvalidation test and regex work

--- back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/tonsoftacos/services/ordersServices/OrdersService.java
+++ back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/tonsoftacos/services/ordersServices/OrdersService.java
@@ -10,12 +10,18 @@ import com.adamstraub.tonsoftacos.tonsoftacos.entities.Customer;
 import com.adamstraub.tonsoftacos.tonsoftacos.entities.OrderItem;
 import com.adamstraub.tonsoftacos.tonsoftacos.entities.Orders;
 import org.apache.commons.lang3.ArrayUtils;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.lang.reflect.MalformedParametersException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -33,10 +39,27 @@ public class OrdersService implements OrdersServiceInterface {
 
     @Override
     @Transactional
-    public ReturnOrderToCustomerDto createOrder(@RequestBody NewOrderDto order){
+    public ReturnOrderToCustomerDto createOrder(@RequestBody @NotNull NewOrderDto order) {
         System.out.println("service");
         System.out.println("new order: " + order);
         System.out.println("order items: " + order.getOrder().getOrderItems());
+//        validation:
+        System.out.println("customer name: " + order.getCustomer().getName().length());
+        System.out.println(order.getCustomer().getName() == null);
+        System.out.println(order.getCustomer().getName().matches("[a-z]|[A-Z]"));
+        System.out.println(Arrays.toString(order.getCustomer().getName().getBytes(StandardCharsets.UTF_8)));
+//        try {
+//        if (!order.getCustomer().getName().matches("[a-z]|[A-Z]]")){
+//            throw new IllegalArgumentException("fuckere");
+//        }
+//        if (order.getCustomer().getName().matches("[a-z]|[A-Z]")) {
+//        if (order.getCustomer().getName() == null || order.getCustomer().getEmail() == null
+//                || order.getCustomer().getPhoneNumber() == null || order.getOrder() == null) {
+//            throw new IllegalArgumentException("The order has incomplete fields," +
+//                    " please double check your work and consult the documentation.");
+//        } else {
+
+
 //        System.out.println(genOrderUid());
         Double orderTotal = 0.00;
 
@@ -47,56 +70,75 @@ public class OrdersService implements OrdersServiceInterface {
 //        System.out.println(newOrder);
         List<OrderItem> orderItems = newOrder.getOrderItems();
         List<GetOrderItemDto> orderItemDtos = new ArrayList<>();
-        for (OrderItem orderItem : orderItems) {
-            orderItem.setTotal(orderItem.getQuantity() *
-                    menuItemRepository.getReferenceById(orderItem.getItemId().getId()).getUnitPrice());
-            orderTotal += orderItem.getTotal();
-        }
-
-        Customer newCustomer = order.getCustomer();
-//        System.out.println(newCustomer);
-        if (customerRepository.findByName(newCustomer.getName()) != null) {
-            newOrder.setCustomerId(customerRepository.findByName(newCustomer.getName()).getCustomerId());
+        try {
+            if (!order.getCustomer().getName().matches("^\\p{L}+[\\p{L}\\p{Pd}\\p{Zs}']*\\p{L}+$|^\\p{L}+$")) {
+                throw new IllegalArgumentException("Customer name incorrectly formatted.");
+//            }
+//        }catch(IllegalArgumentException e) {
+//            System.out.println(e);
         }else{
-            customerRepository.save(newCustomer);
-            newCustomer = customerRepository.findByName(newCustomer.getName());
+                for (OrderItem orderItem : orderItems) {
+                    orderItem.setTotal(orderItem.getQuantity() *
+                            menuItemRepository.getReferenceById(orderItem.getItemId().getId()).getUnitPrice());
+                    orderTotal += orderItem.getTotal();
+                }
+
+                Customer newCustomer = order.getCustomer();
+//        System.out.println(newCustomer);
+                if (customerRepository.findByName(newCustomer.getName()) != null) {
+                    newOrder.setCustomerId(customerRepository.findByName(newCustomer.getName()).getCustomerId());
+                } else {
+                    customerRepository.save(newCustomer);
+                    newCustomer = customerRepository.findByName(newCustomer.getName());
 //            System.out.println(newCustomer);
-            newOrder.setCustomerId(newCustomer.getCustomerId());
-        }
-        newOrder.setOrderItems(orderItems);
-        newOrder.setOrderTotal(orderTotal);
+                    newOrder.setCustomerId(newCustomer.getCustomerId());
+                }
+                newOrder.setOrderItems(orderItems);
+                newOrder.setOrderTotal(orderTotal);
 //        set order uid
-        newOrder.setOrderUid(genOrderUid());
-        System.out.println("Order created.");
-        System.out.println(newOrder.getOrderItems());
-        ordersRepository.save(newOrder);
+                newOrder.setOrderUid(genOrderUid());
+                System.out.println("Order created.");
+                System.out.println(newOrder.getOrderItems());
+                ordersRepository.save(newOrder);
 
-        orderConfirmation = ordersRepository.findByOrderUid(newOrder.getOrderUid());
-        System.out.println("order confirmation: " + orderConfirmation.getOrderItems());
-        customerDto.setCustomerName(newCustomer.getName());
-        customerDto.setOrderUuid(newOrder.getOrderUid());
-        customerDto.setOrderTotal(newOrder.getOrderTotal());
+                orderConfirmation = ordersRepository.findByOrderUid(newOrder.getOrderUid());
+                System.out.println("order confirmation: " + orderConfirmation.getOrderItems());
+                customerDto.setCustomerName(newCustomer.getName());
+                customerDto.setOrderUuid(newOrder.getOrderUid());
+                customerDto.setOrderTotal(newOrder.getOrderTotal());
 
-        orderItems = orderConfirmation.getOrderItems();
-        for (OrderItem orderItem : orderItems){
+                orderItems = orderConfirmation.getOrderItems();
+                for (OrderItem orderItem : orderItems) {
 //            orderItemDtos.add(orderItemDtoConvertor(orderItem));
-            orderItemDtos.add(orderItemDtoConvertor(orderItem));
+                    orderItemDtos.add(orderItemDtoConvertor(orderItem));
+                }
+                customerDto.setOrderItems(orderItemDtos);
+                System.out.println("customer dto: " + customerDto);
+//            return customerDto;
+//            if (!order.getCustomer().getName().matches("[a-z]|[A-Z]]")) new IllegalArgumentException("fuckere");
+            }
+//            throw new IllegalArgumentException("fukinup");
+            }catch(IllegalArgumentException e) {
+            System.out.println(e);
+            throw new IllegalArgumentException();
         }
-        customerDto.setOrderItems(orderItemDtos);
-        System.out.println("customer dto: " + customerDto);
-        return customerDto;
+            return customerDto;
+
     }
+
+
     private GetOrderItemDto orderItemDtoConvertor(OrderItem orderItem){
-        GetOrderItemDto orderItemDto = new GetOrderItemDto();
+            GetOrderItemDto orderItemDto = new GetOrderItemDto();
 //        System.out.println(orderItem);
 //        System.out.println(orderItem.getTotal());
-        orderItemDto.setItemName(menuItemRepository.getReferenceById(orderItem.getItemId().getId()).getItemName());
-        orderItemDto.setQuantity(orderItem.getQuantity());
-        orderItemDto.setTotal(orderItem.getTotal());
-        orderItemDto.setUnitPrice(menuItemRepository.getReferenceById(orderItem.getItemId().getId()).getUnitPrice());
+            orderItemDto.setItemName(menuItemRepository.getReferenceById(orderItem.getItemId().getId()).getItemName());
+            orderItemDto.setQuantity(orderItem.getQuantity());
+            orderItemDto.setTotal(orderItem.getTotal());
+            orderItemDto.setUnitPrice(menuItemRepository.getReferenceById(orderItem.getItemId().getId()).getUnitPrice());
 //        System.out.println(orderItemDto);
 //        System.out.println("orderitemdto: " + orderItemDto);
-        return orderItemDto;
+            return orderItemDto;
+
     }
 
 //    @Override
