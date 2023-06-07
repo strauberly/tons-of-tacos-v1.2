233ba9493edd5d7a63285022b3c96f7884879916
Brian Crookshanks <zeron30@hotmail.com>
2023-06-06 21:17:29 -0700

    jwt exceptions handled by global exception handler

--- back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/tonsoftacos/TonsOfTacosApplication.java
+++ back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/tonsoftacos/TonsOfTacosApplication.java
@@ -1,8 +1,12 @@
 package com.adamstraub.tonsoftacos.tonsoftacos;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @SpringBootApplication
@@ -12,8 +16,19 @@ public class TonsOfTacosApplication {
 		return new ModelMapper();
 	}
 
-	public static void main(String[] args) {
+//	@Bean
+//	HandlerExceptionResolver handlerExceptionResolver() {
+//		return new HandlerExceptionResolver() {
+//			@Override
+//			public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+//				return null;
+//			}
+//		};
+//
+//	}
+	public static void main (String[]args){
 		SpringApplication.run(TonsOfTacosApplication.class, args);
 	}
-
 }
+
+
--- back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/tonsoftacos/config/security/JwtAuthFilter.java
+++ back-end/tons-of-tacos/src/main/java/com/adamstraub/tonsoftacos/tonsoftacos/config/security/JwtAuthFilter.java
@@ -1,21 +1,20 @@
 package com.adamstraub.tonsoftacos.tonsoftacos.config.security;
 import com.adamstraub.tonsoftacos.tonsoftacos.dao.OwnerRepository;
 import com.adamstraub.tonsoftacos.tonsoftacos.services.security.JwtService;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.MalformedJwtException;
+import jakarta.annotation.Resource;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.filters.AddDefaultCharsetFilter;
-import org.apache.coyote.Response;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,9 +24,9 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.handler.HandlerExceptionResolverComposite;
 
 import java.io.IOException;
-import java.security.SignatureException;
 import java.util.Date;
 
 @Component
@@ -36,33 +35,45 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final OwnerRepository ownerRepository;
+@Autowired
+@Qualifier("handlerExceptionResolver")
+@NonNull
+private final HandlerExceptionResolver resolver;
 
+//    public JwtAuthFilter(JwtService jwtService, OwnerRepository ownerRepository, HandlerExceptionResolver resolver) {
+//        this.jwtService = jwtService;
+//        this.ownerRepository = ownerRepository;
+//        this.resolver = resolver;
+//    }
 
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response,
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response,
                                     @NotNull FilterChain filterChain)
             throws ServletException, IOException {
 
 
 try {
-            String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-            String token = null;
-            String username = null;
-            Date expiration = null;
-            Date issuedAt = null;
+    String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+    String token = null;
+    String username = null;
+    Date expiration = null;
+    Date issuedAt = null;
 //        String passwrd = null;
 //        System.out.println(authHeader);
-            if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                token = authHeader.substring(7);
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        token = authHeader.substring(7);
 //            System.out.println("token = " + token);
-                username = jwtService.extractUsername(token);
+        username = jwtService.extractUsername(token);
 //            System.out.println("username = " + username);
-                expiration = jwtService.extractExpiration(token);
-                System.out.println("expiration = " + expiration);
-                issuedAt = jwtService.extractIssuedAt(token);
-                System.out.println("issued at = " + issuedAt);
-            }
+        expiration = jwtService.extractExpiration(token);
+        System.out.println("expiration = " + expiration);
+        issuedAt = jwtService.extractIssuedAt(token);
+        System.out.println("issued at = " + issuedAt);
+    }
+//}catch(Exception e){
+//    resolver.resolveException(request, response, null, e);
+//}
 
         assert expiration != null;
         if (!issuedAt.before(expiration)){
@@ -83,20 +94,22 @@ try {
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
                 System.out.println("jwt filter");
-//                filterChain.doFilter(request, response);
+                filterChain.doFilter(request, response);
             }
 //        filterChain.doFilter(request, response);
-        }catch (ExpiredJwtException e){
-            System.out.println(e.getLocalizedMessage());
-            response.setContentType("application/json");
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            response.getOutputStream().println("{ \"error\": \"token issued after expiration\" }");
-//    response.getOutputStream().println("{error:" +  e.getMessage()  + "}");
-    response.getOutputStream().println(e.getLocalizedMessage());
+//        }catch (ExpiredJwtException e){
+}catch (Exception e){
+    resolver.resolveException(request, response, null, e);
+//            System.out.println(e.getLocalizedMessage());
+//            response.setContentType("application/json");
+//            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+//            response.getOutputStream().println("{ \"error\": \"token issued after expiration\" }");
+////    response.getOutputStream().println("{error:" +  e.getMessage()  + "}");
+//    response.getOutputStream().println(e.getLocalizedMessage());
 //            throw new ExpiredJwtException(e.getHeader(), e.getClaims(), "expired" );
 //    ResponseWrapper responseWrapper = new ResponseWrapper().fail().msg(e.getMessage());
         }
-        filterChain.doFilter(request, response);
+//        filterChain.doFilter(request, response);
     }
 //            throws ServletException, IOException {
 //        String authHeader = request.getHeader("Authorization");
